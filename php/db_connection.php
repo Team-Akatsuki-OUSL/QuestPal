82cd7cb5aa49f7722578144abf2375a80fff51fd
Lakshan Gunasekara <lakshan.idx@gmail.com>
2023-11-07 09:36:42 +0530

    Change online database to local database

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,7 +1,16 @@
 <?php 
-    $hostname = "db4free.net:3306";
+    /* $hostname = "db4free.net:3306";
     $username = "questpaldb";
     $password = "12345678asdfghjkl";
+    $dbname = "questpal";
+    $connect = mysqli_connect($hostname, $username, $password, $dbname);
+    if (!$connect) {
+        echo "Connection failed!";
+    } */
+
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
     $dbname = "questpal";
     $connect = mysqli_connect($hostname, $username, $password, $dbname);
     if (!$connect) {
